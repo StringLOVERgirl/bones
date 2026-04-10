420f48c245b6b1152f08c8f2ecdfb973b725b823
StringLOVERgirl <marchenkostar@gmail.com>
2026-04-10 17:35:30 +0300

    mob v4

--- main.js
+++ main.js
@@ -52,19 +52,19 @@ let interval = false;
         if (tracks[tracknumber].paused) {
           pshide()
           video.pause()
-          playstatus.style.setProperty('--ps', 'url(./noun-play-6054301.svg) center / contain no-repeat')
+          playstatus.style.setProperty('--ps', 'url(./assets//noun-play-6054301.svg) center / contain no-repeat')
           tracks[tracknumber].play()
           others[tracknumber].style.setProperty('--color','rgba(156,163,240,0.78)')
           if (typeof currenttype != 'number') currenttype = tracknumber
           console.log(currenttype + 123)
-          playstatus.style.setProperty('--ps', 'url(./noun-pause-2731843.svg) center / contain no-repeat')
+          playstatus.style.setProperty('--ps', 'url(./assets/noun-pause-2731843.svg) center / contain no-repeat')
 
         }
         else {
           console.log('ap pause')
           pshide()
           tracks[tracknumber].pause()
-          playstatus.style.setProperty('--ps', 'url(./noun-play-6054301.svg) center / contain no-repeat')
+          playstatus.style.setProperty('--ps', 'url(./assets/noun-play-6054301.svg) center / contain no-repeat')
 
         }
         tracks.forEach((e,i)=>{
@@ -84,11 +84,11 @@ let interval = false;
             console.log(e,i)
             e.pause()}
           )
-          playstatus.style.setProperty('--ps', 'url(noun-pause-2731843.svg) center / contain no-repeat')
+          playstatus.style.setProperty('--ps', 'url(./assets/noun-pause-2731843.svg) center / contain no-repeat')
         } else {
           pshide()
           video.pause() 
-          playstatus.style.setProperty('--ps', 'url(noun-play-6054301.svg) center / contain no-repeat')
+          playstatus.style.setProperty('--ps', 'url(./assets/noun-play-6054301.svg) center / contain no-repeat')
          }
       }
     
